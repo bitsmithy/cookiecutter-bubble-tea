aa4269f86d8b0be1a8224a0c474eadae558be2a0
Howard Huang <hao@hwrd.me>
2023-12-18 15:34:20 -0600

    Always initialize with latest Go version

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+asdf install golang latest
+asdf local golang latest
+go mod init '{{ cookiecutter.app_path }}'
 make install
 gofmt -s -w .
 git init && git add . && git commit -m "Initial commit, generated with cookiecutter"
