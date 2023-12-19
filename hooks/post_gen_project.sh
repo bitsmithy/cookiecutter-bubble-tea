ec08a52ec621d395a4af722a69bd7ac0de7b32c9
Howard Huang <hao@hwrd.me>
2023-12-18 22:48:16 -0600

    Use gofumpt and goimports-reviser

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,5 +4,6 @@ asdf install golang latest
 asdf local golang latest
 go mod init '{{ cookiecutter.app_path }}'
 make install
-gofmt -s -w .
+goimports-reviser -rm-unused -set-alias -format ./...
+gofumpt -l -w .
 git init && git add . && git commit -m "Initial commit, generated with cookiecutter"
