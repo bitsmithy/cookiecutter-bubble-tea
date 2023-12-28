d517467e5da7c5e8ea3392d529aac4d685a90464
Howard Huang <hao@hwrd.me>
2023-12-28 11:45:04 -0600

    Add lefthook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@
 asdf install golang latest
 asdf local golang latest
 go mod init '{{ cookiecutter.app_path }}'
-make install
-goimports-reviser -rm-unused -set-alias -format ./...
-gofumpt -l -w .
+make setup
+make audit
 git init && git add . && git commit -m "Initial commit, generated with cookiecutter"
+lefthook install
