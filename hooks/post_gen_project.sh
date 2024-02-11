3371dcc1cb1ee1a1949075da13ec3175b7632e89
Howard Huang <hao@hwrd.me>
2024-02-10 21:55:12 -0600

    Ensure `just` tasks run, even without an existing executable
    Also fail when hooks fail

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 mise install golang@latest
 mise use golang@latest
-go mod init '{{ cookiecutter.app_path }}'
+mise exec golang@latest -- go mod init '{{ cookiecutter.app_path }}'
 make bootstrap
-just lint
-just test
+mise exec just@latest -- just lint
+mise exec just@latest -- just test
 git init && git add . && git commit -m "Initial commit, generated with cookiecutter"
 lefthook install
